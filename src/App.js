f07bed781245892827e31f832737fee549160483
Sarmad <afzalsarmad77@gmail.com>
2022-11-18 02:52:40 -0500

    added shuffling property + assets folder containing images for cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
+//
+const cardCategories = [
+  { src: "./assets/cece.jpg" },
+  { src: "./assets/coach.jpg" },
+  { src: "./assets/jess.jpg" },
+  { src: "./assets/winston.jpg" },
+  { src: "./assets/nick.jpg" },
+  { src: "./assets/schmidt.jpg" },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const shuffle = () => {
+    const isShuffled = [...cardCategories, ...cardCategories]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(isShuffled);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Test Your Memory!</h1>
-      <button> New Game</button>
+      <button onClick={shuffle}> New Game</button>
     </div>
   );
 }
